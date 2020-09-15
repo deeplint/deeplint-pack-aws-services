1a66731672f1c6769fc97c93932fa89330d6b546
Gunay G <ggunay@gmail.com>
2020-09-15 23:30:19 +0200

    new rules

---
+++ src/rules/DAXEncryption.js
@@ -0,0 +1,51 @@
+exports.check = async function (context) {
+    const resources = context.getResources()
+    let problems = []
+    const _ = require('lodash')
+
+
+    for (const key of Object.keys(resources)) {
+        for (const resource of resources[key]) {
+            
+                let isEnabled = false;
+
+            try {
+
+                if (resource.type === 'aws::dax::types::ssedescription') {
+                
+
+
+                    if (_.has(resource.properties, 'status') && resource.properties.status == "ENABLED")
+                    {
+
+                        isEnabled = true
+                        continue 
+                    }
+
+
+                }
+            
+
+
+            }
+            catch(e) {
+
+                    console.error(e.message);
+            }
+        
+            finally{
+
+                if (!isEnabled) {
+                    problems.push({
+                        message: `AWS DAX ${resource.name} does not have the Server Side encryption enabled`
+                    })
+                }
+                    continue
+        
+                }
+        }
+
+    }
+    
+    return problems
+}
---
+++ src/rules/DocDBLogging.js
@@ -0,0 +1,50 @@
+exports.check = async function (context) {
+    const resources = context.getResources()
+    let problems = []
+    const _ = require('lodash')
+    let log_types = ["profiler", "audit"]
+
+    for (const key of Object.keys(resources)) {
+        for (const resource of resources[key]) {
+            
+                let isEnabled = false;
+
+            try {
+
+                if (resource.type === 'aws::docdb::types::dbinstance') {
+                
+
+
+                    if (_.has(resource.properties, 'enabled_cloudwatch_logs_exports') &&  (resource.properties.enabled_cloudwatch_logs_exports).some(v => log_types.includes(v)))
+                    {
+
+                        isEnabled = true
+                        continue 
+                    }
+
+
+                }
+
+
+            }
+            catch(e) {
+
+                    console.error(e.message);
+            }
+        
+            finally{
+
+                if (!isEnabled) {
+                    problems.push({
+                        message: `AWS Doc DB ${resource.name} does not have the proper logging enabled`
+                    })
+                }
+                    continue
+        
+                }
+        }
+
+    }
+    
+    return problems
+}
